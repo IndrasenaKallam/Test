7bb49183ef4af491b0a5579493f224c32f5b7994
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-05 13:54:47 -0800

    python

---
+++ test.py
@@ -0,0 +1,39 @@
+import argparse
+import getpass
+from colorama import Fore, init
+import getpass
+
+from pip._vendor import requests
+from pip._vendor.urllib3.exceptions import InsecureRequestWarning
+
+'''
+parser = argparse.ArgumentParser(description='addition of two number')
+parser.add_argument('-first', '--first_number', help='Enter the first number')
+parser.add_argument('-second', '--second_number', help='Enter the first number')
+opts = parser.parse_args()
+
+print(opts)
+
+'''
+
+#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+init(autoreset=True)
+
+
+class config_gen:
+    username = $username
+    password = $password
+
+    def __init__(self):
+        print(Fore.BLUE + " please provide the credentials: ")
+        #self.username = input("username is({}) : ".format(username)
+        #self.password = getpass.getpass(prompt="PDX-ENG password for %s: " % self.username)
+
+    def getting_arguments(self):
+        print(self.username)
+        print(self.password)
+
+
+if __name__ == '__main__':
+    generator = config_gen()
+    generator.getting_arguments()

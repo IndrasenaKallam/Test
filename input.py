502fc2721ee2ecf517e2a56448134724951a5441
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-09 15:52:11 -0800

    Update input.py

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 import os 
 from os import environ
-a= os.environ['username']
-b= os.environ['password']
-c= os.environ['middle']
+a= os.environ.get('username')
+b= os.environ.get('password')
+c= os.environ.get('middle')
 print(a,b,c)

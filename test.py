646c3be842b4f5fbd6b98b749ccef53cb9af3a42
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-08 20:19:20 -0800

    update

--- test.py
+++ test.py
@@ -19,16 +19,16 @@ class config_gen:
         print(Fore.BLUE + " please provide the credentials: ")
         # self.username = input("username is({}) : ".format(username)
         # self.password = getpass.getpass(prompt="enter the  password for %s: " % self.username)
-        name = os.getenv("middle")
-        print(type(name))
-        left1 = self._physical_rack(name, -1)
-        right1 = self._physical_rack(name, +1)
+        self.name = os.getenv("middle")
+        print(type(self.name))
+        left1 = self._physical_rack(self.name, -1)
+        right1 = self._physical_rack(self.name, +1)
         print(left1)
         print(right1)
-        print(name)
+        print(self.name)
 
     @staticmethod
-    def _physical_rack(self, name, incremental):
+    def _physical_rack(name, incremental):
         return self.name[:-2] + "{0:0=2d}".format(int(self.name[-2:]) + self.incremental)
 
     def getting_arguments(self):

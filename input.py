91edd4ee199f9917b05627e0b2e82caff6aaf6b2
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-09 13:31:17 -0800

    Update input.py

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 import os 
 from os import environ
-a= os.environ['username']
-b= os.environ['password']
-c= os.environ['middle']
+a= os.environ['ramesh1']
+b= os.environ['ramesh2']
+c= os.environ['ramesh_middle']
 print(a,b,c)

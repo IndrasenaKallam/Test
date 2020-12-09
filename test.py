19196f665639115ab25bc5a8ef77f771567f5660
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-08 20:37:51 -0800

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ class config_gen:
         print(Fore.BLUE + " please provide the credentials: ")
         # self.username = input("username is({}) : ".format(username)
         # self.password = getpass.getpass(prompt="enter the  password for %s: " % self.username)
-        name = "h09"
+        name = os.getenv("middle")
         print(type(name))
         left1 = self._physical_rack(name, -1)
         right1 = self._physical_rack(name, +1)

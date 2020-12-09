2af50abdae400975df53db52ab689dd96312af53
indrasena  kallam <59785648+IndrasenaKallam@users.noreply.github.com>
2020-12-08 18:41:39 -0800

    update

--- test.py
+++ test.py
@@ -12,15 +12,24 @@ init(autoreset=True)
 class config_gen:
     username = os.getenv("username")
     password = os.getenv("password")
-    name = {"servers":[]}
-    name1 = {"servers":[]}
+    name = {"servers": []}
+    left1 = {"servers":[]}
+    right1 = {"servers": []}
 
     def __init__(self):
         print(Fore.BLUE + " please provide the credentials: ")
         # self.username = input("username is({}) : ".format(username)
         # self.password = getpass.getpass(prompt="enter the  password for %s: " % self.username)
-        name = os.getenv(name)
+        name = os.getenv("name")
+        left1 = self._physical_rack(name, -1)
+        right1 = self._physical_rack(name, +1)
+        print(left1)
+        print(right1)
+        print(name)
 
+    def _physical_rack(name, incremental):
+        return name[:-2] + "{0:0=2d}".format(int(name[-2:]) + incremental)
+    
     def getting_arguments(self):
         print(self.username)
         print(self.password)
